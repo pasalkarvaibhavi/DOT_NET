7e3f4cc5b950282a1925cb455884f0bade024347
pasalkarvaibhavi <vaibhavipasalkar2004@gamil.com>
2025-03-31 20:17:52 +0530

    Upload

---
+++ ASSIGNMENT/LAB_Based_on_Dot_NET/1_C#_Language_Basics/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_C__Language_Basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            while (true)
+            {
+                Console.WriteLine("\n--- MENU ---");
+                Console.WriteLine("1. Swap First and Last Characters of a String");
+                Console.WriteLine("2. Sum of Digits of a Number");
+                Console.WriteLine("3. Exit");
+                Console.Write("Enter your choice: ");
+
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        Console.Write("Enter a string: ");
+                        string input = Console.ReadLine();
+                        Console.WriteLine("Modified string: " + SwapFirstLast(input));
+                        break;
+
+                    case "2":
+                        Console.Write("Enter a number: ");
+                        if (int.TryParse(Console.ReadLine(), out int number))
+                        {
+                            Console.WriteLine("Sum of digits: " + SumOfDigits(number));
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid input! Please enter a valid number.");
+                        }
+                        break;
+
+                    case "3":
+                        Console.WriteLine("Exiting program...");
+                        return;
+
+                    default:
+                        Console.WriteLine("Invalid choice! Please enter 1, 2, or 3.");
+                        break;
+                }
+            }
+        }
+
+        static string SwapFirstLast(string str)
+        {
+            if (str.Length < 2)
+                return str;
+
+            char[] chars = str.ToCharArray();
+            char temp = chars[0];
+            chars[0] = chars[chars.Length - 1];
+            chars[chars.Length - 1] = temp;
+
+            return new string(chars);
+        }
+
+        static int SumOfDigits(int num)
+        {
+            int sum = 0;
+            while (num > 0)
+            {
+                sum += num % 10;
+                num /= 10;
+            }
+            return sum;
+        }
+    }
+}
+
+/*Output:
+ 
+--- MENU ---
+1. Swap First and Last Characters of a String
+2. Sum of Digits of a Number
+3. Exit
+Enter your choice: 1
+Enter a string: Vaibhavi
+Modified string: iaibhavV
+
+--- MENU ---
+1. Swap First and Last Characters of a String
+2. Sum of Digits of a Number
+3. Exit
+Enter your choice: 2
+Enter a number: 4137
+Sum of digits: 15
+
+--- MENU ---
+1. Swap First and Last Characters of a String
+2. Sum of Digits of a Number
+3. Exit
+Enter your choice:
+*/
---
+++ ASSIGNMENT/LAB_Based_on_Dot_NET/1_C#_Language_Basics/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("1_C#_Language_Basics")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("1_C#_Language_Basics")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6d76d604-10f6-4fe7-acc7-66e7e8f06c03")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
