bdfcb1e59608dcf999145556774877a7ee2b1ecb
pasalkarvaibhavi <vaibhavipasalkar2004@gamil.com>
2025-03-31 20:24:59 +0530

    Upload

---
+++ ASSIGNMENT/LAB_Based_on_Dot_NET/4_College_Fest_Event_Handling/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace _4_College_Fest_Event_Handling
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F);
+            this.label1.ForeColor = System.Drawing.Color.Purple;
+            this.label1.Location = new System.Drawing.Point(238, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(319, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "College Fest Event Handling";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.SpringGreen;
+            this.button1.Location = new System.Drawing.Point(300, 146);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 54);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Total Bill";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.8F);
+            this.label2.ForeColor = System.Drawing.Color.Crimson;
+            this.label2.Location = new System.Drawing.Point(125, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(271, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Enter Total Number of Visitors";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(413, 65);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(183, 52);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(205, 245);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 16);
+            this.label3.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label4.Location = new System.Drawing.Point(193, 229);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 24);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Result";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 510);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ ASSIGNMENT/LAB_Based_on_Dot_NET/4_College_Fest_Event_Handling/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _4_College_Fest_Event_Handling
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class College
+        {
+            public event EventHandler<int> CollegeFestEvent;
+
+            public void OrganizeFest(int visitors)
+            {
+                CollegeFestEvent?.Invoke(this, visitors);
+            }
+        }
+
+        public class Caterers
+        {
+            public int CalculateCateringCost(int visitors)
+            {
+                return visitors * 200; // ₹200 per plate
+            }
+        }
+
+        public class Decorators
+        {
+            public int CalculateDecorationCost(int visitors)
+            {
+                return 10000 + (10 * visitors); // Base ₹10,000 + ₹10 per visitor
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(textBox1.Text, out int visitors) || visitors <= 0)
+            {
+                label4.Text = "Please enter a valid number of visitors.";
+                return;
+            }
+
+            College college = new College();
+            Caterers caterers = new Caterers();
+            Decorators decorators = new Decorators();
+
+            // Subscribing to event
+            college.CollegeFestEvent += (s, v) =>
+            {
+                int cateringCost = caterers.CalculateCateringCost(v);
+                int decorationCost = decorators.CalculateDecorationCost(v);
+              label4.Text = $"Total Catering Cost: ₹{cateringCost}\nTotal Decoration Cost: ₹{decorationCost}\nGrand Total: ₹{cateringCost + decorationCost}";
+            };
+
+            // Trigger event
+            college.OrganizeFest(visitors);
+        }
+    }
+}
